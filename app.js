1abdd8eb9e21b5846ee7f6c6423afdc0a3104c51
ZHANG QIANQIAN <qianqian@waveo.com>
2017-12-03 23:02:50 +0800

    minr

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const SERVER = { host: 'localhost', port: 3000 };
 const server = new Server(SERVER.host, SERVER.port);
 
 server.on('ready', () => {
-	console.log('✅  Server started');
+	console.log('✅  Server started, listening port: 3000');
 });
