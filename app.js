1a19286adc2bd5ab62b1890b068db259adecdb8d
ZHANG QIANQIAN <qianqian@waveo.com>
2017-12-04 09:31:22 +0800

    change port

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const Server = require('./server');
 
-const SERVER = { host: 'localhost', port: 3000 };
+const SERVER = { host: 'localhost', port: 80 };
 
 const server = new Server(SERVER.host, SERVER.port);
 
 server.on('ready', () => {
-	console.log('✅  Server started, listening port: 3000');
+	console.log('✅  Server started, listening port: ' + SERVER.port);
 });
